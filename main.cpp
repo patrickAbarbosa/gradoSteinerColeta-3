85dea0da33e92a6a50701ee78197a0d3d1eb3be8
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2019-03-20 13:12:36 -0300

    Padrao

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Graph Stenier 
+ * 
+ * Local: Federal Univercity of Juiz de Fora
+ * Authors: Leonardo Nunes and Patrick Barbosa
+ * Course: Science Computer
+ * 
+ * Licence: MIT
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Grupo 03 - Steiner com Coleta de Prêmios" << endl;
+  
+  return 0;
+}
