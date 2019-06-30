1efb73982c42ba5f827f709809441c5ae738998a
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2019-06-30 14:09:32 -0300

    Guloso rodando, mas dando floresta

--- src/Class/Guloso.cpp
+++ src/Class/Guloso.cpp
@@ -55,14 +55,16 @@ void Guloso::auxCalculaGuloso(Vertice *atual, int *n)
     // Guarda o endereço do vertice adjacente;
     Vertice *adjacente = aresta->getAdjacente();
     if(adjacente == NULL)
-      cout << "Merda" << endl;
+      cout << "Adjacente = NULL" << endl;
+      cout << "Peso da aresta: " << aresta->getPeso() << endl;
     if(aresta->getPeso() < adjacente->getPeso() || custoSolucao < custoPagar)
     {
-      cout << "IF" << endl;
-      cout << "Aresta com: " << aresta->getAdjacente()->getInfo() << endl;
+      cout << "# if: "<< adjacente->getInfo() << endl;
       int aux = lido(adjacente->getInfo());
-      cout << "Voutou" << endl;
-      cout << "n: " << *n << endl; 
+      cout << "Está no vetor ? aux: " << aux;
+      if(aux >= 0)
+        cout << ", info: " << vetInfoVertice[aux]->getInfo();
+      cout << endl;
       if(aux == -1)
       {
         cout << "ADD: " << adjacente->getInfo() << endl;
@@ -80,10 +82,21 @@ void Guloso::auxCalculaGuloso(Vertice *atual, int *n)
         ultimoNoGuloso++;
         auxCalculaGuloso(adjacente, n);
       }
-      else
+      else if(aux > 0)
       {
         cout << "Está no vetor" << endl;
-        if(aux != 0 && vetArestaIncidente[aux] != NULL && aresta != NULL && aresta->getPeso() < vetArestaIncidente[aux]->getPeso())
+        cout << "Informações da aresta: {" << endl;
+        cout << "\torigem: " << aresta->getOrigem() << ", info: " << aresta->getOrigem()->getInfo() << "," << endl;
+        cout << "\tdestino: " << aresta->getAdjacente() << ", info: " << aresta->getAdjacente()->getInfo() << "," << endl;
+        cout << "\tpeso: " << aresta->getPeso() << endl;
+        cout << "}" << endl;
+        cout << "Vetor de aresta: {" << endl;
+        cout << "\tendereco: " << vetArestaIncidente[aux]<< endl;
+        cout << "\tdestino: " << vetArestaIncidente[aux]->getAdjacente() << endl;
+        cout << "\tdestino: " << vetArestaIncidente[aux]->getAdjacente() << ", info: " << vetArestaIncidente[aux]->getAdjacente()->getInfo() << "," << endl;
+        cout << "\tpeso: " << vetArestaIncidente[aux]->getPeso() << endl;
+        cout << "}" << endl;
+        if(aresta->getPeso() < vetArestaIncidente[aux]->getPeso())
         {
           cout << "Troca Aresta: " << adjacente->getInfo() << endl;
           // Atualiza o custo da solução
@@ -135,7 +148,7 @@ Grafo* Guloso::calculaGuloso(string verticeInicial)
   // adiciona o inicio da AGM
   vetInfoVertice[0] = inicio;
   ultimo = 1;
-  ultimo = 1;
+  ultimoNoGuloso = 1;
   // Calcula AMG
   int n = 0;
   auxCalculaGuloso(inicio, &n);
